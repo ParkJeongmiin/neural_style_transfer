ae59c7778bf6d2f5cf7a65c8d126d948cd10474c
박정민 <pjm14418@gmail.com>
2025-01-17 16:22:01 +0900

    feat: style transfer 모델 아키텍쳐 구현
    #1

--- model.py
+++ model.py
@@ -6,11 +6,23 @@ from torchvision.models import vgg19
 class StyleTransfer(nn.Module):
     def __init__(self):
         super(StyleTransfer, self).__init__()
-        # TODO: pre-trained VGG19 load
-        # TODO: 추출해야 하는 layer 지정
-        pass
+        self.vgg19_model = vgg19(pretrained=True)  # pre trained vgg19 load
+        self.vgg19_features = self.vgg19_model.features
 
-    def forward(self):
-        # TODO: 지정된 layer를 통과한 feature map 추출
-        # TODO: 모델 출력: (content, style)feature map을 담고 있는 list 형태
-        pass
+        # TODO: 추출해야 하는 layer 지정
+        content_layers = []
+        style_laeyrs = []
+
+    def forward(self, x: torch.Tensor, status: str):
+        outputs = []
+
+        if status == "content":
+            for i in range(len(self.vgg19_features)):
+                x = self.vgg19_features[i](x)
+                # TODO: 지정된 번호의 layer를 통과하면 outputs list에 추가
+        elif status == "style":
+            for i in range(len(self.vgg19_features)):
+                x = self.vgg19_features[i](x)
+                # TODO: 지정된 번호의 layer를 통과하면 outputs list에 추가
+
+        return outputs
