e72dc2fb999b2cb88372ec7fd783c18f490fb3a3
박정민 <jeongmin@Jeongmin.>
2025-01-20 15:13:22 +0900

    feat: content loss 구현
    #3

--- loss.py
+++ loss.py
@@ -1,23 +1,21 @@
-# import
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 # Content loss
 class ContentLoss(nn.Module):
-    def __init__(
-        self,
-    ):
+    def __init__(self):
         super(ContentLoss, self).__init__()
-        pass
 
-    def forward(self, input, target):
-        pass
+    def forward(self, input: torch.Tensor, target: torch.Tensor):
+        content_loss = F.mse_loss(input, target)
+        return content_loss
 
 
 # Style loss
 class StyleLoss(nn.Module):
-    def __init__(
-        self,
-    ):
+    def __init__(self):
         super(StyleLoss, self).__init__()
         pass
 
