8db5f5aaaead0123e1a521bc4910633f37aec49d
박정민 <jeongmin@Jeongmin.>
2025-01-20 14:33:02 +0900

    feat: content, style loss 구조 설계
    #3

---
+++ loss.py
@@ -0,0 +1,25 @@
+# import
+
+
+# Content loss
+class ContentLoss(nn.Module):
+    def __init__(
+        self,
+    ):
+        super(ContentLoss, self).__init__()
+        pass
+
+    def forward(self, input, target):
+        pass
+
+
+# Style loss
+class StyleLoss(nn.Module):
+    def __init__(
+        self,
+    ):
+        super(StyleLoss, self).__init__()
+        pass
+
+    def forward(self, input, target):
+        pass
