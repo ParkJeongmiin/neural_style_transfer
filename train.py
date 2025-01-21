55f6bed36e67fa5583c205a03503fe99e9b845a0
박정민 <pjm14418@gmail.com>
2025-01-21 18:45:22 +0900

    feat: 모델 학습 loop 전체 설계
    #9

---
+++ train.py
@@ -0,0 +1,21 @@
+# import
+
+# device 설정
+
+# data(content, style) load
+## data pre processing - to tensor, transform
+
+# model(StyleTransfer) load
+
+# loss(ContentLoss, StyleLoss), optimizer load
+
+# hyperparameter 설정
+
+# trainloop
+## model apply
+## loss calculate
+## optimizer update
+## loss log
+
+## data post processing : tensor to image
+## save generated image
