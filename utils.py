48a99ecadcb9f5efc4706f8e0a006f1628bccb6a
박정민 <pjm14418@gmail.com>
2025-01-21 22:12:27 +0900

    feat: pre processing 함수 구현
    #10

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import torch
 import torchvision.transforms as T
 
 import numpy as np
@@ -8,10 +9,25 @@ VGG19_MEAN = [0.485, 0.456, 0.406]
 VGG19_STD = [0.229, 0.224, 0.225]
 
 
-def pre_processing(self, input):
-    # TODO: image transform : resize, to tensor, normalize
-    # TODO: (ch, h, w) -> (b, ch, h, w)
-    pass
+def pre_processing(image: torch.Tensor) -> torch.Tensor:
+    """
+    이미지를 모델의 입력 형태에 맞게 변환하는 함수
+
+    Args:
+        iamge (torch.Tensor): 모델의 입력하려는 이미지 (ch, h, w)
+
+    Returns:
+        torch.Tensor: (b, ch, h, w) 형태의 이미지
+    """
+    transform = T.Compose(
+        [
+            T.Resize((256, 256)),
+            T.ToTensor(),
+            T.Normalize(mean=VGG19_MEAN, std=VGG19_STD),
+        ]
+    )
+    image_tensor: torch.Tensor = transform(image)
+    return image_tensor.unsqueeze(0)
 
 
 def post_processing():
