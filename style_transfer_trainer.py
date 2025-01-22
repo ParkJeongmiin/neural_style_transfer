4268cfeee6e971d0ca297ba0e453b3e90d53b65f
박정민 <pjm14418@gmail.com>
2025-01-22 16:38:41 +0900

    feat: trainer 구조 설계
    #12

---
+++ style_transfer_trainer.py
@@ -0,0 +1,32 @@
+# import
+
+
+class StyleTransferTrainer:
+    """
+    _summary_
+    """
+
+    def __init__(self, args):
+        pass
+
+    def train(self, args):
+        # device 설정
+
+        # data(content, style) load
+        ## data pre processing - to tensor, transform
+
+        # model(StyleTransfer) load
+
+        # loss(ContentLoss, StyleLoss), optimizer load
+
+        # hyperparameter 설정
+
+        # trainloop
+        ## model apply
+        ## loss calculate
+        ## optimizer update
+        ## loss log
+
+        ## data post processing : tensor to image
+        ## save generated image
+        pass
