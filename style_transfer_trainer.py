546dc8f048cfdc64cedafa8a2a09884f5cbd3dfb
박정민 <pjm14418@gmail.com>
2025-01-22 17:28:55 +0900

    feat: trainer - device, 데이터 설정
    #14

--- style_transfer_trainer.py
+++ style_transfer_trainer.py
@@ -1,4 +1,8 @@
-# import
+import torch
+
+from PIL import Image
+
+from utils import pre_processing, post_processing
 
 
 class StyleTransferTrainer:
@@ -6,14 +10,20 @@ class StyleTransferTrainer:
     _summary_
     """
 
-    def __init__(self, args):
-        pass
+    def __init__(self, content_dir, style_dir, args):
+        self.content_dir = content_dir
+        self.style_dir = style_dir
 
     def train(self, args):
-        # device 설정
+        # -- device setting
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        # data(content, style) load
-        ## data pre processing - to tensor, transform
+        # -- data load
+        content_image = Image.open(self.content_dir)
+        content_image = pre_processing(content_image).to(device)
+
+        style_image = Image.open(self.style_dir)
+        style_image = pre_processing(style_image).to(device)
 
         # model(StyleTransfer) load
 
