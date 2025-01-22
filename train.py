699c346468572edfbc7055eb7239e22a1252eff2
박정민 <88469147+ParkJeongmiin@users.noreply.github.com>
2025-01-22 23:15:23 +0900

    [FEAT] 모델 학습 실행 파일 구현 (#17)
    * feat: 지정된 trainer 초기화 기능 구현
    
    #15
    
    * feat: 학습 및 학습 시간 계산 기능 구현
    
    #15

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 import os
 import argparse
+import datetime
+from importlib import import_module
+from pytz import timezone
 
 
 if __name__ == "__main__":
@@ -34,8 +37,8 @@ if __name__ == "__main__":
     parser.add_argument(
         "--trainer",
         type=str,
-        default="",
-        help="trainer type (default: )",  # 향후 다른 모델을 구현했을 때, 학습 파이프라인 관리를 위해 수정 필요
+        default="style_transfer",
+        help="trainer type (default: style_transfer)",  # 향후 다른 모델을 구현했을 때, 학습 파이프라인 관리를 위해 수정 필요
     )
     parser.add_argument(
         "--lr", type=float, default=1e-3, help="learning rate (default: 1e-3)"
@@ -53,8 +56,28 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
 
-    # trainer 설정
-    # 학습 시작
+    # -- save directory 초기화
+    content_dir = args.content_path
+    style_dir = args.style_path
 
-    # 학습 종료
-    # 학습 소요 시간 계산 및 출력
+    # -- trainer 설정
+    trainer_name = args.trainer.lower()
+    train_module = getattr(
+        import_module(f"{trainer_name}_trainer"), args.trainer + "Trainer"
+    )
+
+    print(f" Use {trainer_name}_trainer ...")
+    trainer = train_module(content_dir, style_dir, args)
+
+    # -- 학습 + 시간 기록
+    start_time = datetime.datetime.now(timezone("Asia/Seoul"))
+    print(f"학습 시작: {str(start_time)[:19]}")
+
+    trainer.train(args)
+
+    end_time = datetime.datetime.now(timezone("Asia/Seoul"))
+    print(f"학습 종료: {str(end_time)[:19]}")
+
+    # -- 학습 소요 시간 계산 및 출력
+    elapsed_time = end_time - start_time
+    print(f"학습 소요 시간: {elapsed_time}")
